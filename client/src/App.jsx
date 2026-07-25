9ab3951c190fe0cc1863193860b598cda4d17962
GIRICKET <98225355+GIRICKET@users.noreply.github.com>
2026-07-25 18:35:29 +0530

    Connect React Frontend with Express backend using Axios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
+import api from "./services/api";
+
 function App() {
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        const fetchMessage = async () => {
+            try {
+                const response = await api.get("/api/hello");
+                setMessage(response.data.message);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchMessage();
+    }, []);
+
     return (
-        <div>
+        <div style={{ padding: "40px" }}>
             <h1>Expense Tracker</h1>
-            <p>Our Full Stack Journey Begins 🚀</p>
+
+            <h2>Backend Message:</h2>
+
+            <p>{message}</p>
         </div>
     );
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 const PORT = 5000;
 
 app.get("/", (req, res) => {
     res.send("Expense Tracker Backend Running");
+});
+
+app.get("/api/hello", (req, res) => {
+    res.json({
+        success: true,
+        message: "Hello from Express!"
+    });
 });
 
 app.listen(PORT, () => {
