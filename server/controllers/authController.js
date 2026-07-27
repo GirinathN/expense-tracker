7114abb826c8a7137c26256862a95deae1262739
GIRICKET <98225355+GIRICKET@users.noreply.github.com>
2026-07-27 16:44:33 +0530

    Implementation of login API and JWT authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 // Test Route
@@ -56,7 +57,95 @@ const registerUser = async (req, res) => {
     }
 };
 
+const loginUser = async (req, res) => {
+
+    try {
+
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+
+            return res.status(400).json({
+                success: false,
+                message: "Please provide email and password."
+            });
+
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email or password."
+            });
+
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email or password."
+            });
+
+        }
+
+        const token = jwt.sign(
+
+            {
+                id: user._id
+            },
+
+            process.env.JWT_SECRET,
+
+            {
+                expiresIn: "7d"
+            }
+
+        );
+
+        res.status(200).json({
+
+            success: true,
+
+            message: "Login successful.",
+
+            token,
+
+            user: {
+
+                id: user._id,
+
+                name: user.name,
+
+                email: user.email
+
+            }
+
+        });
+
+    }
+
+    catch (error) {
+
+        res.status(500).json({
+
+            success: false,
+
+            message: error.message
+
+        });
+
+    }
+
+};
+
 module.exports = {
     authTest,
     registerUser,
+    loginUser
 };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
     authTest,
     registerUser,
+    loginUser
 } = require("../controllers/authController");
 
 router.get("/", authTest);
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 module.exports = router;
